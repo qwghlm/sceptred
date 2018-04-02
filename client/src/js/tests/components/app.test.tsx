1e9f1c4aa0e13e558700bdf9945f93aa4cacba1a
Chris Applegate <github@qwghlm.co.uk>
2018-04-02 13:56:54 +0100

    Start better tests for App

--- client/src/js/tests/components/app.test.tsx
+++ client/src/js/tests/components/app.test.tsx
@@ -9,25 +9,53 @@ Enzyme.configure({ adapter: new Adapter() })
 jest.mock("../../components/map", () => {
 	return { Map : jest.fn(() => "") }
 });
+// jest.mock("../../lib/geocoder", () => {
+// 	return {
+// 		geocode : jest.fn((searchTerm) => {
+// 			return new Promise((resolve, reject) => {
+// 				if (searchTerm == 'Edinburgh') {
+// 					resolve([{name: "Edinburgh, UK", gridReference: "NT27"}]);
+// 				}
+// 				else {
+// 					reject("No places found");
+// 				}
+// 			});
+// 		})
+// 	}
+// });
 
-function fireEvent(target, eventType) {
-	var evt = document.createEvent("Event");
-	    evt.initEvent(eventType, false, true);
-	    target.dispatchEvent(evt);
-}
+jest.useFakeTimers();
 
-test('App component loads and searches correctly', () => {
+test('App component loads unenabled as expected', () => {
 
 	let app = Enzyme.shallow(<App/>);
+	expect(app.state().enabled).toEqual(true);
 
-	let input = app.find('input');
+	app.instance().onInitError();
+	expect(app.state().enabled).toEqual(false);
+});
 
-	expect(app.state().searchTerm).toEqual("");
+test('App component loads error as expected', () => {
 
-	// Enter some text, expect button to become enabled
-	// input.simulate('change', { target: { value: "NT27"} });
-	// expect(app.state().searchTerm).toEqual("NT27");
-	// expect(app.state().buttonEnabled).toEqual(true);
+	let app = Enzyme.shallow(<App/>);
+	app.instance().onLoadError("No WebGL enabled");
+	expect(app.state().errorMessage).toEqual("No WebGL enabled");
+
+});
+
+test('App component loads correctly as expected', () => {
+
+	let app = Enzyme.shallow(<App/>);
+	app.instance().updateMap({gridReference: "NT27"});
+	expect(app.state().gridReference).toEqual("NT27");
+
+});
+
+	// Enter some text, expect autocomplete to display
+	// input.simulate('change', { target: { value: "Edinburgh"} });
+	// expect(app.state().searchTerm).toEqual("Edinburgh");
+    // await jest.runOnlyPendingTimers();
+	// expect(app.state().searchResults.length).toEqual(1);
 
 	// // Click the button, expect state to be loading
 	// button.simulate('click')
@@ -36,47 +64,59 @@ test('App component loads and searches correctly', () => {
 	// app.instance().loadDone();
 	// expect(app.state().loading).toEqual(false);
 
-});
+// });
 
-test('App component loads on keypresses', () => {
+// test('App component returns an empty search correctly', async() => {
 
-	let app = Enzyme.shallow(<App/>);
+// 	let app = Enzyme.shallow(<App/>);
+// 	let input = app.find('input');
+// 	expect(app.state().searchTerm).toEqual("");
 
-	let input = app.find('input');
+// 	// Enter some text, expect autocomplete to display
+// 	input.simulate('change', { target: { value: "xxx"} });
+// 	expect(app.state().searchTerm).toEqual("xxx");
+//     await jest.runOnlyPendingTimers();
+// 	expect(app.state().searchResults.length).toEqual(0);
 
-	// input.simulate('keyup', { target: { value: "NT2" }, keyCode: 50 });
-	// expect(app.state().loading).toEqual(false);
+// });
 
-	// input.simulate('keyup', { target: { value: "NT2" }, keyCode: 13 });
-	// expect(app.state().loading).toEqual(false);
+// 	let app = Enzyme.shallow(<App/>);
 
-	// input.simulate('keyup', { target: { value: "NT27" }, keyCode: 55 });
-	// expect(app.state().loading).toEqual(false);
+// 	let input = app.find('input');
 
-	// input.simulate('keyup', { target: { value: "NT27" }, keyCode: 13 });
-	// expect(app.state().loading).toEqual(true);
+// 	// input.simulate('keyup', { target: { value: "NT2" }, keyCode: 50 });
+// 	// expect(app.state().loading).toEqual(false);
 
-});
+// 	// input.simulate('keyup', { target: { value: "NT2" }, keyCode: 13 });
+// 	// expect(app.state().loading).toEqual(false);
 
-test('App component handles load failure correctly', () => {
+// 	// input.simulate('keyup', { target: { value: "NT27" }, keyCode: 55 });
+// 	// expect(app.state().loading).toEqual(false);
 
-	let app = Enzyme.shallow(<App/>);
+// 	// input.simulate('keyup', { target: { value: "NT27" }, keyCode: 13 });
+// 	// expect(app.state().loading).toEqual(true);
 
-	// app.find('button').simulate('click')
-	// expect(app.state().loading).toEqual(true);
+// });
 
-	// app.instance().loadFailed("Sorry, failed");
-	// expect(app.state().loading).toEqual(false);
-	// expect(app.state().errorMessage).toEqual("Sorry, failed");
+// test('App component handles load failure correctly', () => {
 
-});
+// 	let app = Enzyme.shallow(<App/>);
 
-test('App component handles WebGL failure correctly', () => {
+// 	// app.find('button').simulate('click')
+// 	// expect(app.state().loading).toEqual(true);
 
-	let app = Enzyme.shallow(<App/>);
-	expect(app.state().enabled).toEqual(true);
+// 	// app.instance().loadFailed("Sorry, failed");
+// 	// expect(app.state().loading).toEqual(false);
+// 	// expect(app.state().errorMessage).toEqual("Sorry, failed");
 
-	app.instance().onInitError();
-	expect(app.state().enabled).toEqual(false);
+// });
 
-});
+// test('App component handles WebGL failure correctly', () => {
+
+// 	let app = Enzyme.shallow(<App/>);
+// 	expect(app.state().enabled).toEqual(true);
+
+// 	app.instance().onInitError();
+// 	expect(app.state().enabled).toEqual(false);
+
+// });
