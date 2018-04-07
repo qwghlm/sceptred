ddd87f2bd920bd1f84d1050012b6364a88a32df2
Chris Applegate <github@qwghlm.co.uk>
2018-04-07 20:01:27 +0100

    Get tests working

--- client/src/js/tests/lib/loader.test.ts
+++ client/src/js/tests/lib/loader.test.ts
@@ -2,12 +2,14 @@ import { Loader } from '../../lib/loader';
 
 global.fetch = require('jest-fetch-mock');
 
+const url = '/dummy_url';
+const fallback = JSON.stringify({foo:null});
+
 test('Loader loads as standard', async () => {
 
     fetch.mockResponseOnce('{"foo": 3}');
 
     const loader = new Loader();
-    const url = '/dummy_url';
     const result = loader.load(url);
 
     await result.then((json) => {
@@ -21,7 +23,7 @@ test('Loader loads as standard', async () => {
 
 });
 
-test('Loader rejects repeated calls', async () => {
+test('Loader rejects if a call is repeated before completion', async () => {
 
     fetch.mockResponseOnce('{"foo": 3}');
 
@@ -38,23 +40,21 @@ test('Loader rejects repeated calls', async () => {
 
 });
 
-test('Loader throws correctly on 404', async () => {
+test('Loader throws on any other 4xx', async () => {
 
-    fetch.mockResponseOnce('{}', {status: 404});
+    fetch.mockResponseOnce('{}', {status: 403});
     const loader = new Loader();
-    const url = '/dummy_url';
     const result = loader.load(url);
 
     await expect(result).rejects.toThrow();
 
 });
 
-test('Loader throws correctly on error', async () => {
+test('Loader throws correctly on network error', async () => {
 
-    fetch.mockRejectOnce(new Error("Service unavailable"));
+    fetch.mockRejectOnce(new Error("Connection unavailable"));
 
     const loader = new Loader();
-    const url = '/dummy_url';
     const result = loader.load(url);
 
     await expect(result).rejects.toThrow();
