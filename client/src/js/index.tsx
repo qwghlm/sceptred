0f035a5ac8ebbd335a4be13082f3fcecadd67b39
Chris Applegate <github@qwghlm.co.uk>
2018-04-09 21:59:18 +0100

    Set Raven for production environment only

--- client/src/js/index.tsx
+++ client/src/js/index.tsx
@@ -12,12 +12,19 @@ declare global {
     const Raven: any;
 }
 
-// Configure raven
-Raven.config('https://7ce65e7cf3a348d0acee6762f7c8fc85@sentry.io/305797', {
-    release: version
-}).install()
-
-// Load app
-Raven.context(function () {
+function load() {
     ReactDOM.render(<App/>, document.getElementById('app'));
-});
+}
+
+// Configure raven
+if (window.SCEPTRED_PROD) {
+    Raven.config('https://7ce65e7cf3a348d0acee6762f7c8fc85@sentry.io/305797', {
+        release: version
+    }).install()
+
+    // Load app
+    Raven.context(load);
+}
+else {
+    load();
+}
